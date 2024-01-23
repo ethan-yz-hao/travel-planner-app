a1c2b89329657aa51b6756388367a2057968a3f3
Ethan Hao <hao.yz.ethan@gmail.com>
2024-01-22 23:11:16 -0500

    postGeonames

---
+++ src/server/postGeonames.js
@@ -0,0 +1,13 @@
+async function postGeonames(apiKey, location) {
+    const url = `http://api.geonames.org/searchJSON?q=${location}&maxRows=10&fuzzy=0.5&username=${apiKey}`;
+    try {
+        const response = await fetch(url);
+        const resObjs = await response.json();
+        const resObj = resObjs[0];
+        return {lng: resObj.lng, lat: resObj.lat, toponymName: resObj.toponymName, countryName: resObj.countryName};
+    } catch (error) {
+        console.log('error', error);
+    }
+}
+
+module.exports = postGeonames;
