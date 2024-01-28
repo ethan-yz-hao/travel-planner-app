9a03051d14f984fcd8c8fa383505913fe3b7451a
Ethan Hao <hao.yz.ethan@gmail.com>
2024-01-28 16:50:29 -0500

    drop down form

--- src/client/js/initHTML.js
+++ src/client/js/initHTML.js
@@ -5,7 +5,13 @@ function initializeHTML() {
     const localISOTime = (new Date(nowUTC - timeZoneOffset)).toISOString().slice(0,16);
     startDateTimeElement.value = localISOTime;
     startDateTimeElement.setAttribute('min', localISOTime);
-    console.log(localISOTime);
+
+    const addButton = document.getElementById('add-trip-button');
+    const formContainer = document.getElementById('trip-form-container');
+
+    addButton.addEventListener('click', function() {
+        formContainer.classList.toggle('dropdown-form-active');
+    });
 
     // if ('serviceWorker' in navigator) {
     //     window.addEventListener('load', () => {
